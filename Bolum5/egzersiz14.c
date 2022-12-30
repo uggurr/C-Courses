8b69ed7578a1ca923d5c706af74511985de60dc7
Ugur Yildiz <uggurryldz@gmail.com>
2022-12-30 15:23:19 +0300

    30.12.22 lesson commit

---
+++ Bolum5/egzersiz14.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Bilgisayar tarafından rastgele üretilen N adet sayı 
+bir dizide tutulmaktadır. Dizi içerisinde istenilen 
+bir değerin dizideki sıra numarası girilerek ilgili 
+sırada bulunan dizi değerinin silinmesini sağlayan 
+C programını fonksiyon kullanarak yazınız.*/
+
+int main()
+{
+
+}
